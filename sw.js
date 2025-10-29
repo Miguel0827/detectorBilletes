2fe7a4157c778ddeb1d1d5d22b4af432e531a793
Miguel0827 <Miguelangelmurillod.l.r@gmail.com>
2025-10-28 22:45:13 -0500

    Subiendo archivos iniciales

---
+++ sw.js
@@ -0,0 +1,50 @@
+const CACHE_NAME = 'detector-billetes-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/models/best.onnx'
+];
+
+// Instalación - cachear archivos
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Cache abierto');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Activación - limpiar caches antiguos
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Eliminando cache antiguo:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch - servir desde cache o red
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - retornar respuesta
+        if (response) {
+          return response;
+        }
+        // Sino, hacer fetch a la red
+        return fetch(event.request);
+      }
+    )
+  );
+});
